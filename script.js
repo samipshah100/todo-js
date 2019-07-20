1824d6e1d240e89ba09361490a565ba54eb40f98
Samip Shah <samip227@gmail.com>
2019-07-20 18:20:36 +0530

    git commit -am "fixed height"

--- script.js
+++ script.js
@@ -15,12 +15,12 @@ var itemTotal = 0
 var counter = 0
 
 //fn init
-function addItem(parentId, elementTag, propertiesObj, htmlText )  {
+function addItem(parentId, elementTag, propertiesObj, htmlText) {
   let p = document.getElementById(parentId)
   let newElement = document.createElement(elementTag)
 
   for (const [key, value] of Object.entries(propertiesObj)) {
-    newElement.setAttribute(key,value)
+    newElement.setAttribute(key, value)
   }
 
   newElement.innerHTML = htmlText
@@ -28,22 +28,22 @@ function addItem(parentId, elementTag, propertiesObj, htmlText )  {
   // p.appendChild(newElement)
 
   // this adds at the beginning
-  p.insertBefore(newElement,p.childNodes[0])
+  p.insertBefore(newElement, p.childNodes[0])
 }
 
-function removeItem(nodeId){
+function removeItem(nodeId) {
   //  get digits from the tag name
-  var id = nodeId.replace(/[^0-9]/g,'');
+  var id = nodeId.replace(/[^0-9]/g, '');
   // alert(id)
-  let containerId =  "li" + id
+  let containerId = "li" + id
   let checkboxID = "checkbox" + id
   let item = document.getElementById(containerId)
-  
-  if (confirm("Do you want to delete that item?"))  {
-    
+
+  if (confirm("Do you want to delete that item?")) {
+
     itemTotal--
     itemCountSpan.innerHTML = itemTotal
-  
+
     // if the checkbox for this item is unchecked, then unchecked 
     //count will --. else if it was checked then unchecked count remains same.
     checkboxElement = document.getElementById("checkbox" + id)
@@ -59,8 +59,8 @@ function removeItem(nodeId){
 }
 
 
-function fadeOutEffect(action)  {
-  
+function fadeOutEffect(action) {
+
   //init
   let fadeTarget
 
@@ -70,10 +70,10 @@ function fadeOutEffect(action)  {
   else if (action === "delete") {
     fadeTarget = document.getElementById('deleteContainer')
   }
-  
+
   fadeTarget.style.opacity = 0
 
-  let fadeEffect = setInterval( function()  {
+  let fadeEffect = setInterval(function () {
     // if ( fadeTarget.style.opacity < 0.1)  {
     //   fadeTarget.style.opacity = 1
     //   // alert("opac")
@@ -96,40 +96,40 @@ function fadeOutEffect(action)  {
 
 function sanitarize(string) {
   const map = {
-      '&': '&amp;',
-      '<': '&lt;',
-      '>': '&gt;',
-      '"': '&quot;',
-      "'": '&#x27;',
-      "/": '&#x2F;',
+    '&': '&amp;',
+    '<': '&lt;',
+    '>': '&gt;',
+    '"': '&quot;',
+    "'": '&#x27;',
+    "/": '&#x2F;',
   };
   const reg = /[&<>"'/]/ig;
-  return string.replace(reg, (match)=>(map[match]));
+  return string.replace(reg, (match) => (map[match]));
 }
 
 // focus input
 document.getElementById("todo-input").focus()
 
 // listen for enter
-document.getElementById("todo-input").addEventListener("keyup", function(event) {
-event.preventDefault()
-if (event.keyCode === 13) {
-  document.getElementById("btn").click()
-}
+document.getElementById("todo-input").addEventListener("keyup", function (event) {
+  event.preventDefault()
+  if (event.keyCode === 13) {
+    document.getElementById("btn").click()
+  }
 })
 
 // update vals on checkbox clicks
-document.getElementById('todo-list').addEventListener('click', function(e)  {
+document.getElementById('todo-list').addEventListener('click', function (e) {
   // if checkbox was not chcecked previously, decrease uncheckedTotal
   if (e.target.type === 'checkbox' && e.target.checked === true) {
-    if (uncheckedTotal != 0)  {
+    if (uncheckedTotal != 0) {
       // alert("target.checked ma chu")
       uncheckedTotal--
       uncheckedCountSpan.innerHTML = uncheckedTotal
     }
   }
   // else increase uncheckedTotal
-  else if (e.target.type=== 'checkbox'){
+  else if (e.target.type === 'checkbox') {
     // alert("else ma chu")
     uncheckedTotal++
     uncheckedCountSpan.innerHTML = uncheckedTotal
@@ -139,23 +139,23 @@ document.getElementById('todo-list').addEventListener('click', function(e)  {
 function newTodo() {
   //alert('New TODO button clicked!')
 
-  if (document.getElementById("todo-input").value==="")  {
-    alert ("Hmm ... don't you want to add any to-do item?")
+  if (document.getElementById("todo-input").value === "") {
+    alert("Hmm ... don't you want to add any to-do item?")
     return false
   }
 
   let sanitized_input = sanitarize(document.getElementById('todo-input').value)
-  const item = {text: sanitized_input, checked: false }
+  const item = { text: sanitized_input, checked: false }
   newList.push(item)
   // itemCreated = true
   // if (itemCreated)  {
   //   fadeOutEffect()
   //   itemCreated = false
   // }
-  
+
   fadeOutEffect("add")
 
-  document.getElementById("todo-input").value=""
+  document.getElementById("todo-input").value = ""
 
   // increase counts.
   uncheckedTotal++
@@ -165,13 +165,13 @@ function newTodo() {
 
   // add li todo-container of type li
   //create todoContainerObj first 
-  todoContainerText =""
+  todoContainerText = ""
   // todoContainerId
   todoContainerObj = {
-    "class":"list-group-item",
-    "id": "li"+counter,
+    "class": "list-group-item",
+    "id": "li" + counter,
   }
-  addItem("todo-list", "li", todoContainerObj,todoContainerText)
+  addItem("todo-list", "li", todoContainerObj, todoContainerText)
 
   // add delete btn of type input
   deleteBtnContent = ""
@@ -185,24 +185,24 @@ function newTodo() {
     style: "margin-left:50px; height:25px; width:25px;",
     onClick: "removeItem(this.id)"
   }
-  addItem(todoContainerObj.id,"input",deleteBtnObj,deleteBtnContent)
-  
+  addItem(todoContainerObj.id, "input", deleteBtnObj, deleteBtnContent)
+
   // add todo-text of type span
   todoTextContent = item.text
   todoTextObj = {
     "class": "todo-text",
     "id": "text" + counter,
   }
-  addItem(todoContainerObj.id,"div",todoTextObj,todoTextContent)
+  addItem(todoContainerObj.id, "div", todoTextObj, todoTextContent)
 
   // add checkbox of type input
-  checkboxText=""
+  checkboxText = ""
   checkboxObj = {
     "type": "checkbox",
     "class": "todo-checkbox",
     "id": "checkbox" + counter,
   }
-  addItem(todoContainerObj.id,"input",checkboxObj,checkboxText)
+  addItem(todoContainerObj.id, "input", checkboxObj, checkboxText)
 
   // removeItem(document.getElementById('samlist'))
 
